6ef0eae7dadbf4592011a6274aff7543f7742b38
Bujo <ruya_kuplu@hotmail.com>
2023-01-26 01:15:41 +0300

    İkinci görevimi yaptım

--- src/Gun01/Ogrenci.java
+++ src/Gun01/Ogrenci.java
@@ -2,6 +2,8 @@ package Gun01;
 
 public class Ogrenci {
     public static void main(String[] args) {
-
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Hello World!");
+        }
     }
 }
